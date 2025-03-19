89d71901b2076503a33b925453a740ee8c729f1b
Daniel Simpson <dsimps21@email.cpcc.edu>
2025-03-19 15:03:54 -0400

    Update to Project_daniel_simpson.class
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_daniel_simpson.java
+++ Project_daniel_simpson.java
@@ -9,7 +9,7 @@ public class Project_daniel_simpson
    public static void main(String[] args)
    {
       //Set variables to collect data
-      int age;
+      int age, smoker = 0, nonSmoker = 0;
       String policyNumber, providerName, firstName, lastName, smokingStatus;
       double height, weight;
       
@@ -64,19 +64,41 @@ public class Project_daniel_simpson
          //Close the file
          inputFile.close();
          
-         //Display policy information
-         System.out.printf("\nPolicy Number: %d\n", userPolicy.getPolicyNumber());
-         System.out.printf("Provider Name: %s\n", userPolicy.getProviderName());
-         System.out.printf("Policyholder's First Name: %s\n", userPolicy.getHolderFirstName());
-         System.out.printf("Policyholder's Last Name: %s\n", userPolicy.getHolderLastName());
-         System.out.printf("Policyholder's Age: %d\n", userPolicy.getHolderAge());
-         System.out.printf("Policyholder's Smoking Status: %s\n", userPolicy.getHolderSmokingStatus());
-         System.out.printf("Policyholder's Height: %.1f inches\n", userPolicy.getHolderHeight());
-         System.out.printf("Policyholder's Weight: %.1f pounds\n", userPolicy.getHolderWeight());
-         System.out.printf("Policyholder's BMI: %.2f\n", userPolicy.getHolderBMI());
-         System.out.printf("Policy Price: $%.2f", userPolicy.getPolicyCost());
+         //Use for loop to display contents of ArrayList for policy holders
+         for(int i = 0; i < policyArray.size(); i++)
+         {
+            //Display policy holder information
+            System.out.printf("\nPolicy Number: %s\n", policyArray.get(i).getPolicyNumber());
+            System.out.printf("Provider Name: %s\n", policyArray.get(i).getProviderName());
+            System.out.printf("Policyholder's First Name: %s\n", policyArray.get(i).getHolderFirstName());
+            System.out.printf("Policyholder's Last Name: %s\n", policyArray.get(i).getHolderLastName());
+            System.out.printf("Policyholder's Age: %d\n", policyArray.get(i).getHolderAge());
+            
+            //Create variable to hold smoker status and gather data from array
+            String smokerStat = policyArray.get(i).getHolderSmokingStatus();
+            
+            //Continue display of policy holder information
+            System.out.printf("Policyholder's Smoking Status (smoker/non-smoker): %s\n", smokerStat);
+            System.out.printf("Policyholder's Height: %.1f inches\n", policyArray.get(i).getHolderHeight());
+            System.out.printf("Policyholder's Weight: %.1f pounds\n", policyArray.get(i).getHolderWeight());
+            System.out.printf("Policyholder's BMI: %.2f\n", policyArray.get(i).getHolderBMI());
+            System.out.printf("Policy Price: $%.2f\n", policyArray.get(i).getPolicyCost());
+            
+            //Empty space between policy information and totals
+            System.out.println();
+            
+            //Setup accumulator for smoker & non-smoker policies
+            if(smokerStat.equalsIgnoreCase("smoker"))
+               smoker ++;
+            else
+               nonSmoker ++;
+
+         }
          
-         
+         //Display number of policies for smokers and non-smokers
+         System.out.println("The number of policies with a smoker is: " + smoker);
+         System.out.println("The number of policies with a non-smoker is: " + nonSmoker);
+                  
          
       }//Close try method
       
